8297c221f49fa10d9abccb30222f88ee94acfc24
Alwin Drenth <alwin@vdlp.nl>
2018-08-31 16:21:51 +0200

    Add upgrade logic Adrenth.Redirect -> Vdlp.Redirect

---
+++ updates/20180831_0002_upgrade_from_adrenth_redirect.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vdlp\Redirect\Updates;
+
+use Illuminate\Contracts\Logging\Log;
+use Illuminate\Database\DatabaseManager;
+use October\Rain\Database\Updates\Migration;
+use Throwable;
+use Vdlp\Redirect\Models;
+
+/** @noinspection AutoloadingIssuesInspection */
+
+/**
+ * Class UpgradeFromAdrenthRedirect
+ *
+ * @package Vdlp\Redirect\Updates
+ */
+class UpgradeFromAdrenthRedirect extends Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function up()
+    {
+        /** @var DatabaseManager $database */
+        $database = resolve('db');
+
+        /** @var Log $log */
+        $log = resolve(Log::class);
+
+        $schema = $database->getSchemaBuilder();
+
+        if (!$schema->hasTable('adrenth_redirect_redirects')) {
+            // Skip upgrade migration.
+            $log->info('No upgrade of Vdlp.Redirect needed. Fresh installation.');
+            return;
+        }
+
+        try {
+            $database->transaction(function () use ($database) {
+                $this->disableForeignKeyCheck($database);
+
+                $mapping = [
+                    'adrenth_redirect_categories' => 'vdlp_redirect_categories',
+                    'adrenth_redirect_redirects' => 'vdlp_redirect_redirects',
+                    'adrenth_redirect_redirect_logs' => 'vdlp_redirect_redirect_logs',
+                    'adrenth_redirect_clients' => 'vdlp_redirect_clients',
+                ];
+
+                // language=ignore
+                foreach ($mapping as $from => $to) {
+                    // Make sure newly created tables are empty.
+                    $database->table($to)->delete();
+
+                    // Move data from old tables to new ones.
+                    $database->statement("INSERT INTO `$to` SELECT * FROM `$from`;");
+                }
+
+                // Migrate plugin settings.
+                $database->table('system_settings')
+                    ->where('item', '=', 'vdlp_redirect_settings')
+                    ->delete();
+
+                // language=ignore
+                $database->statement(
+                    'INSERT INTO `system_settings` '
+                    . "SELECT NULL, 'vdlp_redirect_settings', `value` "
+                    . 'FROM `system_settings` '
+                    . "WHERE `item` = 'adrenth_redirect_settings';"
+                );
+
+                $this->enableForeignKeyCheck($database);
+            });
+        } catch (Throwable $e) {
+            $log->critical('Could not upgrade plugin Vdlp.Redirect from Adrenth.Redirect: ' . $e->getMessage());
+        }
+    }
+
+    public function down()
+    {
+        // No migrations to reverse.
+    }
+
+    /**
+     * @param DatabaseManager $database
+     * @return void
+     */
+    private function disableForeignKeyCheck(DatabaseManager $database)
+    {
+        if ($database->getDriverName() === 'sqlite') {
+            $database->raw('PRAGMA foreign_keys = OFF;');
+        }
+
+        if ($database->getDriverName() === 'mysql') {
+            $database->raw('SET FOREIGN_KEY_CHECKS = 0;');
+        }
+    }
+
+    /**
+     * @param DatabaseManager $database
+     * @return void
+     */
+    private function enableForeignKeyCheck(DatabaseManager $database)
+    {
+        if ($database->getDriverName() === 'sqlite') {
+            $database->raw('PRAGMA foreign_keys = ON;');
+        }
+
+        if ($database->getDriverName() === 'mysql') {
+            $database->raw('SET FOREIGN_KEY_CHECKS = 1;');
+        }
+    }
+}
