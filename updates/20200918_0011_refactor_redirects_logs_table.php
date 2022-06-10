bebfdde5ee1b845b046b438ed6edd28afebab945
panki3a <josefpanak@gmail.com>
2022-06-10 18:52:04 +0200

    Update 20200918_0011_refactor_redirects_logs_table.php

--- updates/20200918_0011_refactor_redirects_logs_table.php
+++ updates/20200918_0011_refactor_redirects_logs_table.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
-use October\Rain\Support\Facades\Schema;
+//use October\Rain\Support\Facades\Schema;
 
 class RefactorRedirectsLogTable extends Migration
 {
